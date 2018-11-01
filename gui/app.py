4e77ed69f4569e58ff6535027c97d4308600599e
Alex Tol <atol514@gmail.com>
2018-10-31 18:32:33 -0700

    PAM finder functionality complete!

--- gui/app.py
+++ gui/app.py
@@ -1,6 +1,43 @@
 from appJar import gui
+
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+#the file system now starts from the directory above
+from modules import PAMFinder
+
+
 def holder(btn):
     print('nothing much')
+
+def findPAMSites():
+    strand = app.getTextArea('t1')
+    templateLocations , complementLocations = PAMFinder.get_PAM(strand.upper())
+    templString = ''
+    for item in templateLocations:
+        templString = templString + str(item) + ' '
+    complString = ''
+    for item in complementLocations:
+        complString = complString + str(item) + ' '
+
+    app.setLabel('l12', templString)
+    app.setLabel('l14', complString)
+
+    try:
+        app.showLabel('l11')
+    except:
+        print('widget already shown')
+    try:
+        app.showLabel('l12')
+    except:
+        print('widget already shown')
+    try:
+        app.showLabel('l13')
+    except:
+        print('widget already shown')
+    try:
+        app.showLabel('l14')
+    except:
+        print('widget already shown')
 
 def hideRegion1():
     try:
@@ -13,9 +50,42 @@ def hideRegion1():
     except:
         print('widget already hidden')
 
+    try:    
+        app.removeLabel('l11')
+    except:
+        print('widget already hidden')
+    
+    try:    
+        app.removeLabel('l11')
+    except:
+        print('widget already hidden')
+
+    try:    
+        app.removeLabel('l12')
+    except:
+        print('widget already hidden')
+
+    try:    
+        app.removeLabel('l13')
+    except:
+        print('widget already hidden')
+
+    try:    
+        app.removeLabel('l14')
+    except:
+        print('widget already hidden')
+
 def showRegion1():
     app.addScrolledTextArea('t1',3)
-    app.addButton('Find Pam Sites',holder,4)
+    app.addButton('Find Pam Sites',findPAMSites,4)
+    app.addLabel('l11','Template Strand Pam Sites:',5)
+    app.addLabel('l12','',6)
+    app.addLabel('l13','Sense Strand Pam Sites:',7)
+    app.addLabel('l14','',8)
+    app.hideLabel('l11')
+    app.hideLabel('l12')
+    app.hideLabel('l13')
+    app.hideLabel('l14')
 
 def hideRegion2():
     try:
@@ -58,6 +128,14 @@ app.addRadioButton("option", "Get Best GRNA for region",2)
 app.setRadioButtonChangeFunction("option", changeFunc)
 
 app.addScrolledTextArea('t1',3)
-app.addButton('Find Pam Sites',holder,4)
+app.addButton('Find Pam Sites',findPAMSites,4)
+app.addLabel('l11','Template Strand Pam Sites:',5)
+app.addLabel('l12','',6)
+app.addLabel('l13','Sense Strand Pam Sites:',7)
+app.addLabel('l14','',8)
+app.hideLabel('l11')
+app.hideLabel('l12')
+app.hideLabel('l13')
+app.hideLabel('l14')
 
 app.go()
