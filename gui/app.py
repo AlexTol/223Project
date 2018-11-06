8f8e27600b8a58198197354cd3330b53f439498f
Alex <atol514@gmail.com>
2018-11-06 12:34:53 -0800

    first version of find grna by strand functionality

--- gui/app.py
+++ gui/app.py
@@ -4,10 +4,41 @@ import sys
 sys.path.append("..") # Adds higher directory to python modules path.
 #the file system now starts from the directory above
 from modules import PAMFinder
+from modules import GRNAGenerator
 
 
 def holder(btn):
     print('nothing much')
+
+def getGRNA():
+    templateGRNAs, complementGRNAs = GRNAGenerator.generateGRNAPairs(app.getTextArea('t2').upper())
+    
+    templString = ''
+    for item in templateGRNAs:
+        templString = templString + str(item) + ' '
+    complString = ''
+    for item in complementGRNAs:
+        complString = complString + str(item) + ' '
+
+    app.setLabel('l22', templString)
+    app.setLabel('l24', complString)
+
+    try:
+        app.showLabel('l21')
+    except:
+        print('widget already shown')
+    try:
+        app.showLabel('l22')
+    except:
+        print('widget already shown')
+    try:
+        app.showLabel('l23')
+    except:
+        print('widget already shown')
+    try:
+        app.showLabel('l24')
+    except:
+        print('widget already shown')
 
 def findPAMSites():
     strand = app.getTextArea('t1')
@@ -47,11 +78,6 @@ def hideRegion1():
     
     try:    
         app.removeButton('Find Pam Sites')
-    except:
-        print('widget already hidden')
-
-    try:    
-        app.removeLabel('l11')
     except:
         print('widget already hidden')
     
@@ -105,27 +131,77 @@ def hideRegion3():
 def showRegion3():
     app.addButton('tba2',holder,4)
 
+def showRegion4():
+    app.addScrolledTextArea('t2',4)
+    app.addButton('Get GRNAs',getGRNA,5)
+    app.addLabel('l21','Template Strand Pam Sites:',6)
+    app.addLabel('l22','',7)
+    app.addLabel('l23','Sense Strand Pam Sites:',8)
+    app.addLabel('l24','',9)
+    app.hideLabel('l21')
+    app.hideLabel('l22')
+    app.hideLabel('l23')
+    app.hideLabel('l24')
+
+def hideRegion4():
+    try:
+        app.removeTextArea('t2')
+    except:
+        print('text area already hidden')
+    
+    try:    
+        app.removeButton('Get GRNAs')
+    except:
+        print('widget already hidden')
+
+    try:    
+        app.removeLabel('l21')
+    except:
+        print('widget already hidden')
+
+    try:    
+        app.removeLabel('l22')
+    except:
+        print('widget already hidden')
+
+    try:    
+        app.removeLabel('l23')
+    except:
+        print('widget already hidden')
+
+    try:    
+        app.removeLabel('l24')
+    except:
+        print('widget already hidden')
+
 def changeFunc(rb):
     if(app.getRadioButton("option") == "Find all potential PAM sequences"):
         hideRegion2()
         hideRegion3()
+        hideRegion4()
         showRegion1()
     elif(app.getRadioButton("option") == "Get Best GRNA for gene"):
         hideRegion1()
         hideRegion3()
+        hideRegion4()
         showRegion2()
     elif(app.getRadioButton("option") == "Get Best GRNA for chromosome"):
         hideRegion1()
         hideRegion2()
+        hideRegion4()
         showRegion3()
-        
+    elif(app.getRadioButton("option") == "Get Best GRNA for strand"):
+        hideRegion1()
+        hideRegion2()
+        hideRegion3()
+        showRegion4()
 
 app = gui("CRISPR TOOL","400x400")
 
 app.addRadioButton("option", "Find all potential PAM sequences",0)
 app.addRadioButton("option", "Get Best GRNA for strand",1)
 app.addRadioButton("option", "Get Best GRNA for gene",2)
-app.addRadioButton("option", "Get Best GRNA for region",3)
+app.addRadioButton("option", "Get Best GRNA for chromosome",3)
 app.setRadioButtonChangeFunction("option", changeFunc)
 
 app.addScrolledTextArea('t1',4)
